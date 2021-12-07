f235dde7f2117935962d082b84811ee26c845309
Ivan Iurchenkov <qwerty29544@gmail.com>
2021-12-07 20:41:21 +0300

    Check5

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from volume.discrete_shapes import cube_shape
 import numpy as np
 import random
+import numba
 
 random.seed(132)
 np.random.seed(132)
 
-n = 20
-m = 20
+n = 200
+m = 200
 
 h_x = 1/n
 h_y = 1/m
@@ -14,26 +15,32 @@ h_y = 1/m
 Y = (np.linspace(0, 1, m + 1) + h_y/2)[:-1]
 X = (np.linspace(0, 1, n + 1) + h_x/2)[:-1]
 
-U0 = np.random.uniform(-100, 100, n*m).reshape((n, m))
-U = U0.copy()
+U = np.random.uniform(-100, 100, n*m).reshape((n, m))
 
 steps = 10
 print("\n")
 
+
+@numba.jit(nogil=True, fastmath=True)
 def A(x, y):
     return 1/10 * (x**2 + y**2)
 
+@numba.jit(nogil=True, fastmath=True)
 def f(x1, x2):
     return x1 + x2
 
-for s in range(steps):
-    U0 = U.copy()
-    for i in range(n):
-        for j in range(m):
-            U[i, j] = f(X[i], Y[j])
-            for k in range(n):
-                for l in range(m):
-                    U[i, j] += A(X[i] - X[k], Y[j] - Y[l]) * U0[k, l] * h_x * h_y
-    print(np.sum(np.abs(U - U0)))
-    print("\n")
+@numba.jit(nogil=True, fastmath=True, parallel=True)
+def Solve(U, steps, func, A, X, Y, h_x, h_y):
+    for s in range(steps):
+        U0 = U.copy()
+        for i in range(n):
+            for j in range(m):
+                U[i, j] = func(X[i], Y[j])
+                for k in range(n):
+                    for l in range(m):
+                        U[i, j] += A(X[i] - X[k], Y[j] - Y[l]) * U0[k, l] * h_x * h_y
+        print(np.sum(np.abs(U - U0)))
+        print("\n")
+
+Solve(U, steps, f, A, X, Y, h_x, h_y)
 
--- volume/discrete_shapes/cube_shape.py
+++ volume/discrete_shapes/cube_shape.py
@@ -1,62 +1,49 @@
 import numpy as np
 import numba
+import matplotlib.pyplot as plt
 
 
-@numba.jit(nogil=True, cache=True, parallel=True, fastmath=True)
-def cube_shape(center_point=None,
-               hwl_lengths=None,
-               agt_angles=None,
-               n_discrete_hwl=None):
-    # default value for n argument
-    if n_discrete_hwl is None:
-        n_discrete_hwl = np.array([4, 4, 4], dtype=int)
-    else:
-        n_discrete_hwl = np.array(n_discrete_hwl, dtype=int)
-
-    # default value for angle argument
-    if agt_angles is None:
-        agt_angles = np.array([0., 0., 0.], dtype=float)
-    else:
-        agt_angles = np.array(agt_angles, dtype=float)
-
-    # default value for height, width and length argument
-    if hwl_lengths is None:
-        hwl_lengths = np.array([1., 1., 1.], dtype=float)
-    else:
-        hwl_lengths = np.array(hwl_lengths, dtype=float)
-
-    # default value for center point argument
-    if center_point is None:
-        center_point = np.array([0., 0., 0.], dtype=float)
-    else:
-        center_point = np.array(center_point, dtype=float)
-
+@numba.jit(cache=True, fastmath=True, nogil=True, nopython=True)
+def cube_shape(center_point, hwl_lengths, n_discrete_hwl):
     # Cube tensor for computations in N^3 x 4 x 3 shape
-    cube_tensor = np.zeros(shape=(np.prod(n_discrete_hwl), 8, 3),
-                           dtype=float)
+    cube_tensor = np.zeros((np.prod(n_discrete_hwl), 8, 3))
 
     # Plane tensor for prev computations in XY space
-    plane_tensor = np.zeros(shape=(np.prod(n_discrete_hwl[:2]), 4, 2),
-                            dtype=float)
+    plane_tensor = np.zeros((np.prod(n_discrete_hwl[:2]), 4, 2))
 
     # Norms or proportions of HWL points of cube
-    height = np.arange(start=0, stop=1, step=1 / n_discrete_hwl[0])
-    width = np.arange(start=0, stop=1, step=1 / n_discrete_hwl[1])
-    length = np.arange(start=0, stop=1, step=1 / n_discrete_hwl[2])
+    height = (np.linspace(0, 1, n_discrete_hwl[0]+1) * hwl_lengths[2]) - hwl_lengths[2]/2 + center_point[2]
+    width = (np.linspace(0, 1, n_discrete_hwl[1]+1) * hwl_lengths[1]) - hwl_lengths[1]/2 + center_point[1]
+    length = (np.linspace(0, 1, n_discrete_hwl[2]+1) * hwl_lengths[0]) - hwl_lengths[0]/2 + center_point[0]
 
     # xy points allocation
-    for l_index in np.arange(length.shape[0]):
-        for w_index in np.arange(width.shape[0]):
-            plane_tensor[l_index * width.shape[0] + w_index] = np.array([[length[l_index], width[w_index]],
-                                                                         [length[l_index + 1], width[w_index]],
-                                                                         [length[l_index], width[w_index + 1]],
-                                                                         [length[l_index], width[w_index + 1]]])
+    for l_index in range(length.shape[0]-1):
+        for w_index in range(width.shape[0]-1):
+            plane_tensor[l_index * (width.shape[0]-1) + w_index] = np.array([[length[l_index], width[w_index]],
+                                                                             [length[l_index + 1], width[w_index]],
+                                                                             [length[l_index], width[w_index + 1]],
+                                                                             [length[l_index + 1], width[w_index + 1]]])
 
-    return plane_tensor
+    N_2 = np.prod(n_discrete_hwl[:2])
+    for h_index in range(height.shape[0]-1):
+        lower_matrix = np.concatenate((plane_tensor, np.full((N_2, 4, 1), height[h_index])), axis=2)
+        upper_matrix = np.concatenate((plane_tensor, np.full((N_2, 4, 1), height[h_index+1])), axis=2)
+        cube_tensor[(h_index * N_2):((h_index + 1) * N_2)] = np.concatenate((lower_matrix, upper_matrix), axis=1)
+
+
+
+
+    return cube_tensor
 
 if __name__ == "__main__":
-    cube = cube_shape(center_point=np.array([0., 0., 0.]),
-                      hwl_lengths=np.array([1., 1., 1.]),
-                      agt_angles=np.array([0., 0., 0.]),
-                      n_discrete_hwl=np.array([4, 4, 4]))
-    print(cube)
+    cube = cube_shape(center_point=np.array([1., 1., 1.]),
+                      hwl_lengths=np.array([1., 2., 3.]),
+                      n_discrete_hwl=np.array([8, 8, 8], dtype=np.int64))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    for i in range(len(cube)):
+        print(cube[i])
+        ax.scatter(cube[i][:, 0], cube[i][:, 1], cube[i][:, 2])
+    plt.show()
