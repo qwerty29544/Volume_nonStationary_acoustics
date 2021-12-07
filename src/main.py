ccba3d60caa2dec6e4b203d4d14b6f428ecffb77
Ivan Iurchenkov <qwerty29544@gmail.com>
2021-12-07 19:10:58 +0300

    Check4

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ Y = (np.linspace(0, 1, m + 1) + h_y/2)[:-1]
 X = (np.linspace(0, 1, n + 1) + h_x/2)[:-1]
 
 U0 = np.random.uniform(-100, 100, n*m).reshape((n, m))
-U = U0
+U = U0.copy()
 
 steps = 10
 print("\n")
@@ -34,6 +34,6 @@ for s in range(steps):
             for k in range(n):
                 for l in range(m):
                     U[i, j] += A(X[i] - X[k], Y[j] - Y[l]) * U0[k, l] * h_x * h_y
-    print(np.sum(np.abs(U-U0)))
+    print(np.sum(np.abs(U - U0)))
     print("\n")
 
