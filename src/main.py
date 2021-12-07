dc4785e4940b08b44f641afa5edcfd43b64360c6
Ivan Iurchenkov <qwerty29544@gmail.com>
2021-12-07 19:06:02 +0300

    Check1

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from volume.discrete_shapes import cube_shape
 import numpy as np
+import random
+
+random.seed(132)
+np.random.seed(132)
 
 n = 20
 m = 20
@@ -10,27 +14,26 @@ h_y = 1/m
 Y = (np.linspace(0, 1, m + 1) + h_y/2)[:-1]
 X = (np.linspace(0, 1, n + 1) + h_x/2)[:-1]
 
-U = np.ones((n, m))
+U0 = np.random.uniform(-100, 100, n*m).reshape((n, m))
+U = U0
 
 steps = 10
-
-print(U)
-
 print("\n")
 
 def A(x, y):
-    return 1/5 * (x**2 + y**2)
+    return 1/10 * (x**2 + y**2)
 
 def f(x1, x2):
     return x1 + x2
 
 for s in range(steps):
+    U0 = U
     for i in range(n):
         for j in range(m):
             U[i, j] = f(X[i], Y[j])
             for k in range(n):
                 for l in range(m):
-                    U[i, j] += A(X[i] - X[k], Y[j] - Y[l]) * U[k, l] * h_x * h_y
-    print(U)
+                    U[i, j] += A(X[i] - X[k], Y[j] - Y[l]) * U0[k, l] * h_x * h_y
+    print(np.max(U-U0))
     print("\n")
 
