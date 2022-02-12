0cb7a887f3b900805bf5f23195b4bca3fca0ee2f
Ivan Iurchenkov <qwerty29544@gmail.com>
2022-02-12 16:42:43 +0300

    ThreeSGD.py Не закончен

--- iterations_lib/python_inspectors/ThreeSGD.py
+++ iterations_lib/python_inspectors/ThreeSGD.py
@@ -20,24 +20,26 @@ def ThreeSGD_solver(complex_matrix: np.ndarray,
     if u0_vector is None:
         u0_vector = np.ones(f_vector.shape[0], dtype=complex)
 
-    it_space = np.zeros((1,), dtype=float)
+    it_space = np.zeros((3,), dtype=float)
 
     # Формирование матрицы истории итераций
-    u_vector = np.zeros((1, len(u0_vector)), dtype=complex)
+    u_vector = np.zeros((3, len(u0_vector)), dtype=complex)
     u_vector[0] = u0_vector.copy()
 
     # Явное указание комплексности матрицы
     complex_matrix = np.array(complex_matrix, dtype=complex)
     f_vector = np.array(f_vector, dtype=complex)
 
-    r = np.zeros((1, complex_matrix.shape[0]), dtype=complex)
-    delta_r = np.zeros((1, complex_matrix.shape[0]), dtype=complex)
-    g = np.zeros((1, complex_matrix.shape[0]), dtype=complex)
-    d = np.zeros((1, complex_matrix.shape[0]), dtype=complex)
+    r = np.zeros((2, complex_matrix.shape[0]), dtype=complex)
+    delta_r = np.zeros((2, complex_matrix.shape[0]), dtype=complex)
+    g = np.zeros((2, complex_matrix.shape[0]), dtype=complex)
+    d = np.zeros((2, complex_matrix.shape[0]), dtype=complex)
 
     alpha = np.zeros((2,), dtype=float)
     beta = np.zeros((2,), dtype=float)
     gamma = np.zeros((2,), dtype=float)
+
+    # Первая итерация ----------------------------------------------------------------------------------
 
     H_star = np.transpose(np.conj(complex_matrix))
 
@@ -46,12 +48,36 @@ def ThreeSGD_solver(complex_matrix: np.ndarray,
     H_g_0 = complex_matrix @ g[0]
     beta[0] = utils.vec_dot_complex_prod(g[0], g[0]) / utils.vec_dot_complex_prod(H_g_0, H_g_0)
 
-    new_u = u_vector[0] - beta[0] * g[0]
-    u_vector = np.concatenate((u_vector, new_u.reshape((1, -1))), axis=0)
+    u_vector[1] = u_vector[0] - beta[0] * g[0]
 
-    it_space = np.concatenate((it_space, np.array(3).reshape((1,))), axis=0)
+    it_space[1] = 3
 
-    for iter_index in range(1, n_iter):
+    # Вторая итерация ----------------------------------------------------------------------------------
+
+    r[1] = complex_matrix @ u_vector[1] - f_vector
+    delta_r[1] = r[1] - r[0]
+    g[1] = H_star @ r[1]
+    H_g_0 = complex_matrix @ g[1]
+
+    l_11 = utils.vec_dot_complex_prod(delta_r[1], delta_r[1])
+    l_12 = utils.vec_dot_complex_prod(g[1], g[1])
+    l_21 = l_12.copy()
+    l_22 = utils.vec_dot_complex_prod(H_g_0, H_g_0)
+    c_1 = 0
+    c_2 = l_12.copy()
+
+    det_L = l_11 * l_22 - l_12 * l_21
+    det_L1 = c_1 * l_22 - l_12 * c_2
+    det_L2 = l_11 * c_2 - c_1 * l_21
+
+    alpha[1] = det_L1 / det_L
+    beta[1] = det_L2 / det_L
+
+    u_vector[2] = u_vector[1] - alpha[1] * (u_vector[1] - u_vector[0]) - beta[1] * g[1]
+
+    it_space[2] = 3
+
+    for iter_index in range(2, n_iter):
         new_r = complex_matrix @ u_vector[iter_index] - f_vector
         r = np.concatenate((r, new_r.reshape((1, -1))), axis=0)
 
